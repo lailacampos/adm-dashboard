6089ac25fa9cc7f21cddb36d607ca359c96da1df
Laila <lailarhcampos@gmail.com>
2025-02-16 20:00:49 -0300

    add index file to export Header, Sidebar, StatCard, StatCardContainer, and ThemeSwitcher components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from './layout/Header';
+export { default as Sidebar } from './layout/Sidebar';
+export { default as StatCard } from './ui/StatCard';
+export { default as StatCardContainer } from './ui/StatCardContainer';
+export { default as ThemeSwitcher } from './ui/ThemeSwitcher';
