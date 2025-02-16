e36f757d3a72ade54d84288c655cebc1228803a8
Laila <lailarhcampos@gmail.com>
2025-02-16 18:25:30 -0300

    refactor OverviewPage to use StatCardContainer for dynamic stat card rendering based on route

--- src/pages/OverviewPage/OverviewPage.tsx
+++ src/pages/OverviewPage/OverviewPage.tsx
@@ -1,33 +1,22 @@
-import { motion } from "framer-motion";
-import StatCard from "../../components/StatCard";
-import { useStatCardsItems } from "../../data/statCardsItems";
+import { useLocation } from "react-router-dom";
+import Paths from "../../routes/paths";
+import StatCardContainer from "../../components/StatCardContainer";
+import React from "react";
 
-const OverviewPage = () => {
+type RouteKey = keyof typeof Paths;
 
-    const STAT_CARDS_ITEMS = useStatCardsItems();
+const OverviewPage: React.FC = () => {
+
+    const location = useLocation();
+    const routeKey: RouteKey = (location.pathname === "/" ? "overview" :
+        Object.keys(Paths).find(key => Paths[key] === location.pathname as RouteKey) || "overview"
+    );
 
     return (
         <>
             <div className="flex-1 overflow-auto relative z-10">
                 <main className="max-w-7xl mx-auto py-20 px-4 lg:px-8">
-                    <motion.div
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1 }}
-                        className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
-                    >
-                        {Object.values(STAT_CARDS_ITEMS["overview"]).map((item, index) => (
-                            <StatCard
-                                key={index}
-                                name={item.name}
-                                icon={item.icon}
-                                color={item.color}
-                                value={Math.floor(Math.random() * 1000)}
-                            />
-                        ))
-
-                        }
-                    </motion.div>
+                    <StatCardContainer category={routeKey} />
                 </main>
             </div>
         </>
