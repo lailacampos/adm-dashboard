92f4098807447bdb7d3b4dcca710000f642f6b80
Laila <lailarhcampos@gmail.com>
2025-02-16 03:05:50 -0300

    replace ThemeSwitcher with Navbar and Sidebar components in MainLayout for improved layout structure

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
-import ThemeSwitcher from "../components/ThemeSwitcher";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
     return (
-        <div className="p-6 flex h-screen overflow-hidden
-            bg-gray-100 text-gray-900
+        <div className="flex h-screen overflow-hidden
+            bg-gray-50 text-gray-800
             dark:bg-gray-900 dark:text-gray-100">
                 <div className="fixed inset-0 z-0">
                     <div className="absolute inset-0 bg-gradient-to-br
                         dark:from-gray-950 dark:via-gray-800 dark:to-gray-950 dark:opacity-80" />
                     <div className="absolute inset-0 backdrop-blur-sm" />
                 </div>
-            <ThemeSwitcher />
+            <Navbar />
+            <Sidebar />
             <Outlet />
         </div>
     );
