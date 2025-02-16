a40e778729a75c820482d8bd1cc2b115a6e8d438
Laila <lailarhcampos@gmail.com>
2025-02-16 03:52:25 -0300

    add routes for Users, Sales, Orders, Analytics, and Settings pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,11 @@ import MainLayout from '../layouts/MainLayout';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 import OverviewPage from '../pages/OverviewPage/OverviewPage';
 import ProductsPage from '../pages/ProductPage/ProductsPage';
+import Users from '../pages/Users/Users';
+import Sales from '../pages/Sales/Sales';
+import Orders from '../pages/Orders/Orders';
+import Analytics from '../pages/Analytics/Analytics';
+import Settings from '../pages/Settings/Settings';
 
 const AppRoutes = () => {
     return (
@@ -12,6 +17,11 @@ const AppRoutes = () => {
                 <Route element={<MainLayout />}>
                     <Route path={Paths.home} element={<OverviewPage />} />
                     <Route path={Paths.products} element={<ProductsPage />} />
+                    <Route path={Paths.users} element={<Users />} />
+                    <Route path={Paths.sales} element={<Sales />} />
+                    <Route path={Paths.orders} element={<Orders />} />
+                    <Route path={Paths.analytics} element={<Analytics />} />
+                    <Route path={Paths.settings} element={<Settings />} />
                     <Route path="*" element={<NotFoundPage />} />
                 </Route>
             </Routes>
