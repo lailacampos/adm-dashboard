1ca08f25a3abee5402fa7a71a999ff89b107451d
Laila <lailarhcampos@gmail.com>
2025-02-22 23:42:26 -0300

    feat: adjust OverviewPage layout and include SalesOverviewChart component

--- src/pages/OverviewPage/OverviewPage.tsx
+++ src/pages/OverviewPage/OverviewPage.tsx
@@ -14,12 +14,12 @@ const OverviewPage: React.FC = () => {
     return (
         <>
             <div className="flex-1 overflow-auto relative z-10">
-                <main className="max-w-7xl mx-auto py-20 px-4 lg:px-8">
+                <main className="max-w-350 mx-auto py-20 px-4 lg:px-8">
                     <StatCardContainer category={routeKey} />
 
                     {/* CHARTS */}
                     <div className="grid grid-cols-1 lg-grid-cols-2 gap-8">
-
+                        <SalesOverviewChart />
                     </div>
                 </main>
             </div>
